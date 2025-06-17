5623cd33de920e9f6cae804231c6dfcf5aa3c1c7
gabeFrancisco <gabegit2@gmail.com>
2025-06-17 15:43:07 -0300

    stylized header component

--- web/checklist-app/src/app/header/header.component.ts
+++ web/checklist-app/src/app/header/header.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
